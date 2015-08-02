8e4ea2de541c673db4c5282a5d64e08fde86d72f
ning zhou <zhouning8620@gmail.com>
2015-08-01 23:46:33 -0500

    Create dropbox_chooser_1_makeTheButtonWork.js

---
+++ dropbox_chooser_1_makeTheButtonWork.js
@@ -0,0 +1,4 @@
+/after creating the button, the key is to make the button works so as to choose files from your dropbox.
+/You should expect that after clicking the button, you can log in your dropbox and drag files to the website.
+/However, since on a tumblr.com blog site, the purpose of the button will not help you to build the website. It is adding files to your blog
+/that is the dropbox chooser might not fit for tumblr.com web design.
